f43ecfd69418f3812f518aa6c11c9481b68a6947
Anton14889 <jalssomino@gmail.com>
2020-03-12 22:34:01 +0200

    fixed style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, HostListener, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { ResizedEvent } from 'angular-resize-event';
 
@@ -9,9 +9,8 @@ import * as jsPDF from 'jspdf'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements OnInit {
-  @ViewChild('example', { static: true }) example;
-
+export class AppComponent {
+ 
   width: number;
   height: number;
   x: number;
@@ -21,36 +20,17 @@ export class AppComponent implements OnInit {
   canvasSrc: string;
 
 
-  constructor(
-    private _el: ElementRef,
-
-  ) { }
 
   onResized(event: ResizedEvent) {
     this.width = event.newWidth;
     this.height = event.newHeight;
   }
-
-  ngOnInit() {
-
-  }
-
-
-  savePdf() {
-    var doc = new jsPDF();
-    var example = document.getElementById("example") as HTMLCanvasElement,
-      ctx = example.getContext('2d');
-    doc.addImage(this.canvasSrc, 'JPEG', 15, 40, 180, 160)
-    doc.save('a4.pdf')
-  }
-
+ 
 
   public onDragEnded(event): void {
     this.x = event.source.getFreeDragPosition().x;
     this.y = event.source.getFreeDragPosition().y;
   }
-
-
 
   cutImg() {
 
@@ -66,28 +46,32 @@ export class AppComponent implements OnInit {
     pic.width = this.width - 20;
     pic.height = this.height - 20;
 
+    let x = -this.x || 0;
+    let y = -this.y || 0;
 
-    let x = this.x || 0;
-    let y = this.y || 0;
-
-    x = -x;
-    y = -y;
-
-    let left = cont.offsetLeft + 11 - x - ex.offsetLeft;
-    let top = cont.offsetTop + 11 - y - ex.offsetTop;
-    let xx = left - 300;
-    let yy = top - 100;
-
+    let left = cont.offsetLeft + 11 - x - ex.offsetLeft -300;
+    let top = cont.offsetTop + 11 - y - ex.offsetTop -100;
+   
     pic.onload = () => {
       ctx.clearRect(0, 0, 200, 200);
-      ctx.drawImage(pic, xx, yy, pic.width, pic.height);
+      ctx.drawImage(pic, left, top, pic.width, pic.height);
       this.canvasSrc = example.toDataURL();
     }
     pic.src = img.src;
 
   }
+  
 
-  aa() {
+  savePdf() {
+    var doc = new jsPDF();
+    var example = document.getElementById("example") as HTMLCanvasElement,
+      ctx = example.getContext('2d');
+    doc.addImage(this.canvasSrc, 'JPEG',  0, 0, 100, 100)
+    doc.save('a4.pdf')
+  }
+
+
+  loadImg() {
     this.src = '';
     var inp = document.getElementById("inp") as HTMLInputElement;
     var reader = new FileReader();
